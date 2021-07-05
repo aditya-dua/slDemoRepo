9d4cec686a4de466d4e16a1b0a716967361ce561
Nimisha Mehta <nimishamehta@Nimishas-MacBook-Air.local>
2021-07-05 21:29:24 +0530

    Changes have been committed.

---
+++ event_loop.js
@@ -0,0 +1,29 @@
+let request = require('request')
+
+let apiKey='3d99c4a247cde93b9708fe2700904ae2';
+
+const CITIES=['Kolkata','Banglore','Delhi'];
+
+const city = CITIES[Math.floor(Math.random()*CITIES.length)];
+
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+request(url,function(err,response,body){
+    console.log('URL : '+url);
+
+    console.log('Fetching the weather data for city : '+city);
+
+    if(err){
+        console.log('Theres an error getting weather data.')
+    }else{
+        let weather = JSON.parse(body)
+        let message = `It's ${weather.main.temp} degress in ${weather.name}! `;
+        console.log(message);
+    }
+    greet();
+});
+//JSON.parse(body) & JSON.stringify
+
+function greet(){
+    console.log('Weather data is fecthed!');
+}
